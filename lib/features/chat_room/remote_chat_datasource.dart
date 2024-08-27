cd49edbc25aba7c3f194e2a50ef9be743fd678a3
Eka Nova <pramudya.eka@nolimit.id>
2024-08-27 16:44:09 +0700

    refactor: Add lazy loading for images in chat room

--- lib/features/chat_room/remote_chat_datasource.dart
+++ lib/features/chat_room/remote_chat_datasource.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:convert';
 
 import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
 
 import 'package:flutter_ollama_integration/features/chat_room/models/chat_model.dart';
 
@@ -9,8 +10,11 @@ import '../../clients/locator_injector.dart';
 import '../../services/secure_storage.dart';
 
 abstract class ChatDatasource {
-  Stream<ChatResponseModel> getChatResponseFromServer(
+  Stream<ChatResponseModel> getChatResponseStream({required String userInput});
+
+  Future<ChatResponseModel> getChatResponseNonStream(
       {required String userInput});
+
   void abortCurrentRequest();
 }
 
@@ -24,8 +28,9 @@ class RemoteChatDatasource implements ChatDatasource {
   );
 
   @override
-  Stream<ChatResponseModel> getChatResponseFromServer(
-      {required String userInput}) async* {
+  Stream<ChatResponseModel> getChatResponseStream({
+    required String userInput,
+  }) async* {
     final storageService = locator<SecureStorageImpl>();
 
     try {
@@ -39,6 +44,7 @@ class RemoteChatDatasource implements ChatDatasource {
         data: {
           "model": baseModel,
           "prompt": userInput,
+          "stream": true,
         },
         options: Options(responseType: ResponseType.stream),
         cancelToken: cancelToken,
@@ -59,6 +65,45 @@ class RemoteChatDatasource implements ChatDatasource {
         yield ChatResponseModel(response: "Request cancelled", done: true);
       } else {
         yield ChatResponseModel(response: "Error: ${e.toString()}", done: true);
+      }
+    }
+  }
+
+  @override
+  Future<ChatResponseModel> getChatResponseNonStream(
+      {required String userInput}) async {
+    final storageService = locator<SecureStorageImpl>();
+    try {
+      final baseUrl = await storageService.readBaseUrl();
+      final basePort = await storageService.readPort();
+      final basePath = await storageService.readCustomPath();
+      final baseModel = await storageService.readCustomModel();
+
+      final response = await _dio.post(
+        'http://$baseUrl:$basePort$basePath',
+        data: {
+          "model": baseModel,
+          "prompt": userInput,
+          "stream": false,
+        },
+        cancelToken: cancelToken,
+      );
+      debugPrint("Response: ${response.data}");
+
+      if (response.statusCode == 200) {
+        return ChatResponseModel.fromJson(response.data);
+      } else {
+        return ChatResponseModel(
+          response: "Error: ${response.statusMessage}",
+          done: true,
+        );
+      }
+    } catch (e) {
+      if (e is DioException && e.type == DioExceptionType.cancel) {
+        return ChatResponseModel(response: "Request cancelled", done: true);
+      } else {
+        return ChatResponseModel(
+            response: "Error: ${e.toString()}", done: true);
       }
     }
   }
